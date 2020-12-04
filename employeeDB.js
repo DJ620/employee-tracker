1708f1bfac9142265b1b2ec1c6c2f6d7b2bb3a73
DJ Hersh <DanielJHersh@gmail.com>
2020-12-03 18:02:10 -0800

    created mysql server and began writing code

--- employeeDB.js
+++ employeeDB.js
@@ -1,2 +1,29 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Hersh6624!",
+    database: "employeeDB"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    begin();
+});
+
+const begin = () => {
+    console.log("Welcome to the Employee Database.");
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "How may I assist you?",
+            choices: ["Add employee information", "Update employee information", "View employee information", "Delete employee information"],
+            name: "choice"
+        }
+    ]).then((response) => {
+
+    })
+}
