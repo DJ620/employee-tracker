015fceb05e53c05b199577ff5facd83cfed853af
DJ Hersh <DanielJHersh@gmail.com>
2020-12-08 19:58:12 -0800

    Commented my db.js file

--- db.js
+++ db.js
@@ -8,49 +8,58 @@ const connection = mysql.createConnection({
     password: "Hersh6624!",
     database: "employeeDB"
 });
+
 connection.connect((err) => {
     if (err) throw err;
 });
+
+// Promisifying the query allows me to keep this code in a seperate file and still run asynchronously
 connection.query = util.promisify(connection.query);
+
+// A class to hold useful methods
 class DB {
   // Keeping a reference to the connection on the class in case we need it later
   constructor(connection) {
     this.connection = connection;
   };
-
+    // Returns all data from any table passed in
     tableInfo(table) {
         return this.connection.query(`SELECT * FROM ${table}`);
     };
-
+    // Allows for a query of specific data from any table passed in
     customInfo(column, table) {
         return this.connection.query(`SELECT ${column} FROM ${table}`);
     };
-
+    // Returns all employee's who's role title includes the word "Manager"
     viewManagers() {
         return this.connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role ON employee.role_id = role.id WHERE role.title REGEXP 'Manager?'");
     };
 
+    // Returns all roles for any department that gets passed in
     rolesByDepartment(department) {
         return this.connection.query("SELECT role.title, role.salary FROM role LEFT JOIN department ON department.id = role.department_id WHERE department.name = ?", department);
     };
 
+    // Returns all employee information from the three tables
+    // The two parameters are optional and have a default value of 'null' in case they are not needed
     innerJoin(param = null, info = null) {
         let statement = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id";
+        // If the parameters are passed in, a 'WHERE' statement is added to the query to get specific sets of employees
         if (param) {
             statement += ` WHERE ${param} = ?`;
         };
         return connection.query(statement, info);
     };
-
+    // Adds data to any table passed in
     addInfo(table, info) {
         return this.connection.query(
             `INSERT INTO ${table} SET ?`, info);
     };
-
+    // Updates data for any table that gets passed in
     updateInfo(table, params) {
         return this.connection.query(`UPDATE ${table} SET ? WHERE ?`, params);
     };
-
+    // Deletes data from any table that gets passed in
     deleteInfo(table, info) {
         return this.connection.query(`DELETE FROM ${table} WHERE ?`, info);
     };
